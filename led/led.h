7e8ddd036ba0e3760545cc6a3097afcc3dff1310
Ian A Wilson <ian.a.wilson@rochester.edu>
2012-05-06 12:14:52 -0400

    Initial Commit

---
+++ led/led.h
@@ -0,0 +1,29 @@
+#ifndef LED_H
+#define LED_H
+
+#define DISPLAYS_ACROSS 1
+#define DISPLAYS_DOWN 2
+
+///////////////////////////////
+
+#include <DMD.h>
+
+#define PIXELS_WIDE ((DISPLAYS_ACROSS * DMD_PIXELS_ACROSS))
+#define PIXELS_TALL ((DISPLAYS_DOWN * DMD_PIXELS_DOWN))
+#define PIXELS_TOTAL ((PIXELS_WIDE * PIXELS_TALL))
+
+class ImgDMD : public DMD {
+	public:
+		ImgDMD(byte panelsWide, byte panelsHigh);
+		void drawArray(int* array, byte bGraphicsMode);
+		void clearAndDrawArray(int* array, byte bGraphicsMode);
+		void drawFilledCircle(int xCenter, int yCenter, int radius, byte bGraphicsMode);
+		void posSquareTo(int* array, int dlay);
+		void negSquareTo(int* array, int dlay);
+		void posCircleTo(int* array, int dlay);
+	private:
+		void drawFilledCircleSub(int cx, int cy, int x, int y, byte bGraphicsMode);
+		int pythag(int a, int b);
+};
+
+#endif
